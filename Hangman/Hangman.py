62e95a4d5937c7fdb750fbbb657461df7adc72c8
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-01-07 23:44:22 +0530

    Hangman game using python

---
+++ Hangman/Hangman.py
@@ -0,0 +1,82 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+failed = 6
+wordlist = ['apple', 'banana', 'cucumber']
+chosen_word = wordlist[random.randint(0, 2)]
+display = []
+for i in range(0, len(chosen_word)):
+    display += "_"
+print(display)
+end_of_game = False
+while "_" in display:
+    guess = input("Enter your guess:").lower()
+    for letter in range(0, len(chosen_word)):
+        if chosen_word[letter] == guess:
+            display[letter] = guess
+
+    if guess not in display:
+        print(stages[failed])
+        if failed == 0:
+            break
+        failed -= 1
+    print(display)
+
+if "_" in display:
+    print("You Lose!")
+else:
+    print("You Won!!!")
