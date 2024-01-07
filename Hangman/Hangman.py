bd006394784788c6f92b585b327cab77a8be0c24
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-01-07 23:57:37 +0530

    Added Comments

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,14 +1,21 @@
+# Importing libraries and art file
 import random
 from art import stages
 
-failed = 6
-wordlist = ['apple', 'banana', 'cucumber']
+failed = 6   # Number of chances ... Must be equal to the stages in art file 
+wordlist = ['apple', 'banana', 'cucumber']   # Words to guess
+
+# Using random module to choose a word for wordlist
 chosen_word = wordlist[random.randint(0, 2)]
+
 display = []
 for i in range(0, len(chosen_word)):
-    display += "_"
+    display += "_"    # To create empty spaces 
+    
 print(display)
-end_of_game = False
+
+# end_of_game = False
+
 while "_" in display:
     guess = input("Enter your guess:").lower()
     for letter in range(0, len(chosen_word)):
