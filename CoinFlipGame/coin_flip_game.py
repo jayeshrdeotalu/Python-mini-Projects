9268e5f4349bd04763e955c373cfe0f7c0c07289
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-01-08 23:46:38 +0530

    added comments

--- CoinFlipGame/coin_flip_game.py
+++ CoinFlipGame/coin_flip_game.py
@@ -1,8 +1,16 @@
+# Importing libraries
 import random
-guess = input("Enter your Guess")
-guess_int = random.randint(0, 1)
+
+guess = input("Enter your Guess...(Heads or Tails)")
+guess = guess.lower()
 options = ['Heads', 'Tails']
+
+# choosing a ramdom index.
+guess_int = random.randint(0, 1)
+
+# Display the computer choosen side.
 print(f"Flipped side is {options[guess_int]}")
+
 if options[guess_int] == guess:
     print("you Won")
 else:
