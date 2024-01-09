d49e2d98bf514d574c6decbe2f208cfcbcd08b43
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-01-10 01:12:19 +0530

    Caeser Cipher Game

---
+++ Caeser Cipher/caeser_cipher.py
@@ -0,0 +1,45 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+direction = input("Type 'Incode' or 'Decode'\n").lower()
+text = input("Enter your text here:\n").lower()
+shift = int(input("Enter the swift you want:\n"))
+
+
+def caesar(given_text, told_shift, cipher):
+    new_text = ""
+    for letter in given_text:
+        if letter not in alphabet:
+            new_text += letter
+            continue
+        elif cipher == "incode":
+            index = alphabet.index(letter) + told_shift
+            if index > 25:
+                index -= 26
+        else:
+            index = alphabet.index(letter) - told_shift
+            if index < 0:
+                index += 25
+        new_text += alphabet[index]
+    print(f"Here is the {cipher} result: {new_text}")
+
+
+caesar(text, shift, direction)
