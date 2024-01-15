319d060e1ee082a22150a7b8c07a59d344b32503
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-01-15 18:16:15 +0530

    added comments

--- Treasure Map/Treasure_map.py
+++ Treasure Map/Treasure_map.py
@@ -1,8 +1,12 @@
+# Making 3x3 matrix
+
 row1 = ["⬜️", "⬜️", "⬜️"]
 row2 = ["⬜️", "⬜️", "⬜️"]
 row3 = ["⬜️", "⬜️", "⬜️"]
 
+# nested list
 mapping = [row1, row2, row3]
+
 print(f"{row1}\n{row2}\n{row3}")
 print("Enter the two digit number where you want to enter the 'x'")
 map_in = input("First number is row and second is column")
